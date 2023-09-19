ae4f8679e93fbdd8a1d08e4349abdab0e5d5c76b
Jacob McKitrick <jakemckit@gmail.com>
2023-09-19 11:54:26 -0400

    added client dir

---
+++ client/src/js/database.js
@@ -0,0 +1,35 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+  openDB('jate', 1, {
+    upgrade(db) {
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
+        return;
+      }
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
+    },
+  });
+
+export const putDb = async (content) => {
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const req = store.put({ id: 1, value: content });
+
+  const res = await req;
+  console.log('Saved', res);
+};
+
+export const getDb = async () => {
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const req = store.getAll();
+
+  const res = await req;
+  console.log(res);
+};
+
+initdb();
